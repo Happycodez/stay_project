b359c505b67f01a872ae049c478ee5da325c4359
happycodez <happy>
2024-01-20 17:37:04 +0200

    first commit

---
+++ stay_backend/schemas/index.ts
@@ -0,0 +1,7 @@
+import user from './user'
+import pin from './pin'
+import comment from './comments'
+import postedBy from './postedBy'
+import save from './save'
+
+export const schemaTypes = [ user, pin, comment, postedBy, save ]
